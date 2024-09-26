210403881e5adca2d13eb11c056b2f1e9c5519e0
mac <mac@MacBook-Pro.local>
2024-09-26 17:06:21 +0100

    change endpoint

--- app/story/page.jsx
+++ app/story/page.jsx
@@ -23,7 +23,7 @@ function StoryPageContent() {
     async function fetchStory() {
       setLoading(true);
       try {
-        const response = await fetch('http://127.0.0.1:8000/api/story', {
+        const response = await fetch('https://out.bouchikhi.com/api/story', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
